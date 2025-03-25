71e4615fa7e46a352f33f2303e4dff51d2a2655d
UnEmotioneD <blackeagle10@icloud.com>
2025-03-25 21:01:04 +0900

    Add Student class

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,31 @@
 
 using namespace std;
 
+class Student {
+  public:
+    int no;
+    string name;
+    int kor;
+    int eng;
+    int math;
+    int total;
+    float avg;
+    char grade;
+    int rank;
+};
+
 int main() {
+    Student student;
 
-  // read file
-  ifstream theFile;
-  theFile.open("./student_scores.txt");
+    // read file
+    ifstream theFile;
+    theFile.open("./student_scores.txt");
 
-  string studentScores = "";
+    string studentScores = "";
 
-  while (getline(theFile, studentScores)) {
-    cout << studentScores;
-  }
+    while (getline(theFile, studentScores)) {
+        cout << studentScores << endl;
+    }
 
-  cout << studentScores + "\n" << endl;
-
-  return 0;
+    return 0;
 }
