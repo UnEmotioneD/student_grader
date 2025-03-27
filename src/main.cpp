5970777f15b44b9a130f51263f7a572154d98475
UnEmotioneD <blackeagle10@icloud.com>
2025-03-27 13:43:14 +0900

    read file to student instance and add instance to list
    format

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <fstream>
 #include <iostream>
+#include <list>
 #include <string>
 
 using namespace std;
@@ -25,10 +26,22 @@ int main() {
     ifstream stdFile;
     stdFile.open("./student_scores.txt");
 
+    list<Student> studentList;
+
     // assign contents of file to string variable
     string stdInfo = "";
+    int studentCnt = 0;
     while (getline(stdFile, stdInfo)) {
         cout << stdInfo << endl;
+        student.name = stdInfo;
+        studentList.push_back(student);
+        studentCnt++;
+    }
+
+    cout << endl << "Number of students: " << studentCnt << endl;
+
+    for (auto s : studentList) {
+        cout << s.name << endl;
     }
 
     return 0;
